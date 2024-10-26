a923460059dc9862a4299e3ff6a39819fc786490
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-10-26 13:28:09 +0000

    投稿データ保存

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,14 +5,36 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Profile;
+
 class ProfileController extends Controller
 {
 public function add()
 {
     return view('admin.profile.create');
 }
-public function create()
+public function create(Request $request)
 {
+            $this->validate($request, Profile::$rules);
+
+            $profile = new Profile;
+            $form = $request->all();
+    
+            if (isset($form['image'])) {
+                $path = $request->file('image')->store('public/image');
+                $profile->image_path = basename($path);
+            } else {
+                $profile->image_path = null;
+            }
+    
+            // フォームから送信されてきた_tokenを削除する
+            unset($form['_token']);
+            // フォームから送信されてきたimageを削除する
+            unset($form['image']);
+    
+            // データベースに保存する
+            $profile->fill($form);
+            $profile->save();
     return redirect('admin/profile/create');
 }
 public function edit()
