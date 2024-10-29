43278cdf134f9c507ae72022f4eba1187f270675
hashimoto yuzu <you.zoo.0318@gmail.com>
2024-10-29 10:43:02 +0000

    プロフィール編集

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -26,14 +26,27 @@ public function create(Request $request)
             // データベースに保存する
             $profile->fill($form);
             $profile->save();
+
     return redirect('admin/profile/create');
 }
-public function edit()
+public function edit(Request $request)
 {
-    return view('admin.profile.edit');
+    $profile = Profile::find($request->id);
+    if (empty($profile)) {
+        abort(404);
+    }
+    return view('admin.profile.edit', ['profile_form' => $profile]);  
 }
-public function update()
+
+public function update(Request $request)
 {
+    $this->validate($request, Profile::$rules);
+    $profile = Profile::find($request->id);
+    $profile_form = $request->all();
+    unset($profile_form['_token']);
+
+    $profile->fill($profile_form)->save();
+
     return redirect('admin/profile/edit');
 }
 }
